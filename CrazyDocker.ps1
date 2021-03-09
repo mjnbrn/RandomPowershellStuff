ef127eab0be4ea5919771e01546934025d5acf54
Benjamin Pendygraft <mjnbrn@gmail.com>
2021-03-09 00:18:33 -0500

    Create CrazyDocker.ps1

---
+++ CrazyDocker.ps1
@@ -0,0 +1,7 @@
+$Wordlist = (Invoke-restmethod "https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt") -split "`n"
+$Word = Get-Random $Wordlist
+$SearchResults = docker search --limit 100 $word
+$DockerName = (docker search --limit 100 --format "{{json . }}" $word |ConvertFrom-Json |Get-Random  |Select name).name
+while ($DockerName -in (Get-Content .\CrazyDocker.txt)){$DockerName = (docker search --limit 100 --format "{{json . }}" $word |ConvertFrom-Json |Get-Random  |Select name).name}
+docker pull $DockerName
+docker run $DockerName
