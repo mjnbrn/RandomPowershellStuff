15fabd74cd3ca131989ef31a1ead83b25f8604d3
Benjamin Pendygraft <mjnbrn@gmail.com>
2021-03-09 00:24:23 -0500

    Update CrazyDocker.ps1
    backgrounding `docker run`

--- CrazyDocker.ps1
+++ CrazyDocker.ps1
@@ -4,4 +4,4 @@ $SearchResults = docker search --limit 100 $word
 $DockerName = (docker search --limit 100 --format "{{json . }}" $word |ConvertFrom-Json |Get-Random  |Select name).name
 while ($DockerName -in (Get-Content .\CrazyDocker.txt)){$DockerName = (docker search --limit 100 --format "{{json . }}" $word |ConvertFrom-Json |Get-Random  |Select name).name}
 docker pull $DockerName
-docker run $DockerName
+& docker run $DockerName
